c6cbe965abc30636c1d3c24a0d08412c4c4bd48f
wavecao <wavecao1022@gmail.com>
2020-07-10 15:28:00 +0800

    Hystrix 图形化监控面板

---
+++ cloud-consumer-hystrix-dashboard9001/src/main/java/com/cheerway/springcloud/HystrixDashboardMain9001.java
@@ -0,0 +1,21 @@
+package com.cheerway.springcloud;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.cloud.netflix.hystrix.dashboard.EnableHystrixDashboard;
+
+/**
+ * <p>
+ *
+ * </p>
+ *
+ * @author : 曹威
+ * @since : 2020-07-10  9:37
+ */
+@SpringBootApplication
+@EnableHystrixDashboard
+public class HystrixDashboardMain9001 {
+    public static void main(String[] args) {
+        SpringApplication.run(HystrixDashboardMain9001.class, args);
+    }
+}
